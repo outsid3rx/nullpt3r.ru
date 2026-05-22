d76c9bd1454d27e242a77a84b4b2816673bb179a
Max Zernyaev <winterzz@internet.ru>
2026-05-22 14:28:52 +0300

    feat: добавить якори к заголовкам

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,28 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 export default defineConfig({
   site: 'https://nullpt3r.ru',
+
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          properties: {
+            class: 'heading-anchor',
+            ariaHidden: 'true',
+            tabIndex: -1,
+          },
+        },
+      ],
+    ],
+  },
 
   integrations: [tailwind(), mdx(), sitemap()],
 });
